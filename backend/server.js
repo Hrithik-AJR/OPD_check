1e6a90d1153c689ed1b922dab26b539b8715fc41
prakalpshakya31 <prakalpshakya31@gmail.com>
2021-09-11 19:58:00 +0530

    added middlewares

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,11 @@ import dotenv from 'dotenv'
 import connectDB from './config/db.js'
 import colors from 'colors'
 import productRoutes from './routes/productRoutes.js'
+import {
+  notFound,
+  errorHandler,
+} from './middleware/errorMiddleware.js'
+import { error } from 'console'
 
 dotenv.config()
 
@@ -15,6 +20,10 @@ app.get('/', (req, res) => {
 })
 
 app.use('/api/products', productRoutes)
+
+app.use(notFound)
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 
