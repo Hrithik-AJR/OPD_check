084a55d922be63efa364faa8d83f6ef3dc224c0a
prakalpshakya31 <prakalpshakya31@gmail.com>
2021-09-13 16:07:31 +0530

    user login functionality

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -1,6 +1,8 @@
 import {
   USER_LOGIN_REQUEST,
   USER_LOGIN_SUCCESS,
+  USER_LOGIN_FAIL,
+  USER_LOGOUT
 } from '../constants/userConstants'
 import axios from 'axios'
 
@@ -39,3 +41,8 @@ export const login =
       })
     }
   }
+
+export const logout = () => (dispatch) => {
+  localStorage.removeItem('userInfo')
+  dispatch({ type: USER_LOGOUT })
+}
