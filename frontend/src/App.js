c6eb77784a8cc375be35da60d00c3e8e1f1c2a71
prakalpshakya31 <prakalpshakya31@gmail.com>
2021-09-14 21:08:32 +0530

    payment screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,6 +12,7 @@ import LoginScreen from './screens/LoginScreen'
 import RegisterScreen from './screens/RegisterScreen'
 import ProfileScreen from './screens/ProfileScreen'
 import ShippingScreen from './screens/ShippingScreen'
+import PaymentScreen from './screens/PaymentScreen'
 
 const App = () => {
   return (
@@ -27,6 +28,10 @@ const App = () => {
           <Route
             path='/shipping'
             component={ShippingScreen}
+          />
+          <Route
+            path='/payment'
+            component={PaymentScreen}
           />
           <Route
             path='/profile'
