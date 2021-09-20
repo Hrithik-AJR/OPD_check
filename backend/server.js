04b4a046b75157716bdf473ba4406ecc6634d974
prakalpshakya31 <prakalpshakya31@gmail.com>
2021-09-20 11:39:26 +0530

    image upload

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import path from 'path'
 import dotenv from 'dotenv'
 import connectDB from './config/db.js'
 import colors from 'colors'
@@ -9,6 +10,7 @@ import {
   notFound,
   errorHandler,
 } from './middleware/errorMiddleware.js'
+import uploadRoutes from './routes/uploadRoutes.js'
 import { error } from 'console'
 
 dotenv.config()
@@ -26,9 +28,17 @@ app.get('/', (req, res) => {
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/orders', orderRoutes)
+app.use('/api/upload', uploadRoutes)
 
 app.get('/api/config/paypal', (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID)
+)
+
+// make upload folder static
+const __dirname = path.resolve()
+app.use(
+  '/uploads',
+  express.static(path.join(__dirname, '/uploads'))
 )
 
 app.use(notFound)
