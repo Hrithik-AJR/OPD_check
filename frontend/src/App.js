6b09a16c9a99aa6a596cb825edddcccb12a81535
prakalpshakya31 <prakalpshakya31@gmail.com>
2021-09-20 10:25:51 +0530

    edit product screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -18,7 +18,7 @@ import OrderScreen from './screens/OrderScreen'
 import UserListScreen from './screens/UserListScreen'
 import UserEditScreen from './screens/UserEditScreen'
 import ProductListScreen from './screens/ProductListScreen'
-
+import ProductEditScreen from './screens/ProductEditScreen'
 
 const App = () => {
   return (
@@ -67,6 +67,10 @@ const App = () => {
           <Route
             path='/admin/user/:id/edit'
             component={UserEditScreen}
+          />
+          <Route
+            path='/admin/product/:id/edit'
+            component={ProductEditScreen}
           />
           <Route path='/' component={HomeScreen} exact />
         </Container>
